b6fc47982b84e4a551a80988eddac9dad28edb22
“rupeshat1988” <rupeshat1988@gmail.com>
2025-03-31 15:30:51 +0530

    Update plugin details as per need

--- customer-reviews-form.php
+++ customer-reviews-form.php
@@ -1,10 +1,18 @@
 <?php
-/*
-Plugin Name: Customer Review Forms
-Description: A plugin to manage and display reviews.
-Version: 1.0
-Author: Lalit Kumar
-*/
+/**
+ * Plugin Name: Customer Reviews Form
+ * Plugin URI: 
+ * Description: A plugin to manage and display custom reviews.
+ * Version: 1.0
+ * Requires at least: 2.6
+ * Requires PHP: 5.6
+ * Author: Bible Society
+ * Author URI:
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Text Domain: custom-reviews-form
+ */
 
 if (!defined('ABSPATH')) {
     exit;
@@ -18,6 +26,7 @@ require_once CRF_PLUGIN_DIR . 'includes/class-review-assets.php';
 require_once CRF_PLUGIN_DIR . 'includes/class-review-handler.php';
 
 function crf_review_form_shortcode() {
+    // echo "Rupesh is working on Short Code";
     ob_start(); ?>
     <div id="crf-review-form-container"></div>
     <?php return ob_get_clean();
